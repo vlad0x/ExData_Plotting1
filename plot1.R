894d139dac2e7f322d7110783319a1810b256a83
vlad0x <vladtik@outlook.com>
2015-02-08 10:19:00 -0800

    1st plot

---
+++ plot1.R
@@ -0,0 +1,16 @@
+data <- read.table("C:/vlad/household_power_consumption.txt", header=T, sep=";", 
+                   col.names=c("Date", "Time", "Global_active_power", "Global_reactive_power", 
+                               "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", 
+                               "Sub_meterbing_3"))
+
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+
+start_date <- as.Date("2007-02-01")
+stop_date <- as.Date("2007-02-02")
+data <- data[data$Date >= start_date & data$Date <= stop_date,]
+
+data <- data[data$Global_active_power != "?",]
+data$Global_active_power <- as.numeric(as.character(data$Global_active_power))
+
+with(data, hist(Global_active_power, col="red", main="Global Active Power", 
+                xlab="Global Active Power (kilowatts)"))
